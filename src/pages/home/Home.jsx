2cc0d97349e452978ef9061895a7215fc039e142
IrtazaAhmed147 <irtazaahmedk@gmail.com>
2025-06-24 19:58:44 +0500

    basic header done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
+import { Header } from '../../components/header/Header'
 import { Navbar } from '../../components/navbar/Navbar'
+// import { Header } from '../../components/header/Header'
+
 import './home.css'
 
 export const Home = () => {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   )
 }
