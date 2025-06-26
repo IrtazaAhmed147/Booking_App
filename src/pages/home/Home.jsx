6a9155e27b6cf4635876e205b25e39a849912d38
IrtazaAhmed147 <irtazaahmedk@gmail.com>
2025-06-26 10:25:49 -0700

    Homepage list

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,9 @@
+
+import Featured from '../../components/featured/Featured'
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
 import { Header } from '../../components/header/Header'
 import { Navbar } from '../../components/navbar/Navbar'
+import PropertyList from '../../components/propertyList/PropertyList'
 // import { Header } from '../../components/header/Header'
 
 import './home.css'
@@ -9,6 +13,15 @@ export const Home = () => {
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+        {/* <MailList /> */}
+        {/* <Footer /> */}
+      </div>
     </div>
   )
 }
