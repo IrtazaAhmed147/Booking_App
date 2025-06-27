a627c33ec077d22e2e34cf350b819e3115811349
IrtazaAhmed147 <irtazaahmedk@gmail.com>
2025-06-27 12:02:37 +0500

    Hotel Page design

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./index.css"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Home } from './pages/home/Home'
 import { List } from './pages/list/List'
-import { Hotel } from './pages/hotel/Hotel'
+import  Hotel  from './pages/hotel/Hotel'
 
 function App() {
 
